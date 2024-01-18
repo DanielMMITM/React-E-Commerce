3a06a931144df7b12659f4c2be551c7aa2290556
Edgar Martínez <edgar.danmm@gmail.com>
2024-01-18 00:30:09 -0600

    Fix imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'
+import './index.css';
+import AppRouter from './routes/AppRouter.jsx';
+import { RouterProvider } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
