0e563b7d2b8b08928645392f43dd4c3253d2b77e
Edgar Martínez <edgar.danmm@gmail.com>
2024-01-18 22:12:35 -0600

    add navbar and scrollrestoration

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,36 +1,40 @@
+import { ScrollRestoration } from 'react-router-dom';
 import './Login.css';
-import {Button} from '/src/components/Button/Button';
+import { Button } from '/src/components/Button/Button';
+import {NavBar} from '/src/components/NavBar/NavBar';
 
 export function Login() {
     
     return (
-        <div className='mainContainer'>
-            <div className="containerForm">
-                <div id='content'>
-                    <h1>Welcome </h1>
-                    <form action='get'>
-                        <div className='fields'>
-                            <legend>Username</legend>
-                            <input type="text" name='username' placeholder="Username"/>
+        <>
+            <ScrollRestoration/>
+            <NavBar/>
+            <div className='mainContainer'>
+                <div className="containerForm">
+                    <div id='content'>
+                        <h1>Welcome </h1>
+                        <form action='get'>
+                            <div className='fields'>
+                                <legend>Username</legend>
+                                <input type="text" name='username' placeholder="Username"/>
+                            </div>
+                            <div className='fields'>
+                                <legend>Password</legend>
+                                <input type="password" name='password' placeholder='Password'/>
+                            </div>
+                        </form>
+                        <a href="#" id='resetPassword'>Forgot your password?</a>
+                        <div className='btnContainer'>
+                            <Button
+                            route="/"
+                            text= "Login"
+                            >
+                            </Button>
                         </div>
-                        <div className='fields'>
-                            <legend>Password</legend>
-                            <input type="password" name='password' placeholder='Password'/>
-                        </div>
-                    </form>
-                    <a href="#" id='resetPassword'>Forgot your password?</a>
-                    <div className='btnContainer'>
-                        <Button
-                        route="/"
-                        text= "Login"
-                        >
-                        </Button>
                     </div>
                 </div>
             </div>
-        </div>
-            
-        
+        </>
     )
 }
 
