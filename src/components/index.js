bf6277fc68bb4bd770f12d250ccdc9a553b424fe
Edgar Martínez <edgar.danmm@gmail.com>
2024-01-19 11:53:25 -0600

    export components

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export {Button} from "/src/components/Button/Button.jsx";
+export {Footer} from "/src/components/Footer/Footer.jsx";
+export {NavBar} from "/src/components/NavBar/NavBar.jsx";
+export {CardProduct} from "/src/components/CardProduct/CardProduct.jsx";
+export {CardInfo} from "/src/components/CardInfo/CardInfo.jsx";
