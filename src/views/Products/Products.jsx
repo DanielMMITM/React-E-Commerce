10435e45fb4a5fd03ac9c16e3866d22f300b7736
Edgar Martínez <edgar.danmm@gmail.com>
2024-01-19 16:26:42 -0600

    add filter section in products page

--- src/views/Products/Products.jsx
+++ src/views/Products/Products.jsx
@@ -12,16 +12,30 @@ import ReactPaginate from "react-paginate";
 
 
 export function Products({ itemsPerPage }) {
+    const [filterCategory, setFilterCategory] = useState("");
     const [products, setProducts] = useState([]);
     const [itemOffset, setItemOffset] = useState(0);
     const endOffset = itemOffset + itemsPerPage;
     const currentItems = products.slice(itemOffset, endOffset);
     const pageCount = Math.ceil(products.length / itemsPerPage);
 
+    function onFilterValueSelected(filterValue) {
+        setFilterCategory(filterValue);
+    }
+
+    let filteredProductsList = products?.filter((product) => {
+        return product.category === filterCategory;     
+    });
+
     const handlePageClick = (event) => {
         const newOffset = (event.selected * itemsPerPage) % products.length;
         setItemOffset(newOffset);
     };
+
+    const handleFilterChange = e => {
+        setFilterValue(e.target.value);
+    };
+
 
     useEffect(() => {
         fetch('https://fakestoreapi.com/products')
@@ -41,26 +55,51 @@ export function Products({ itemsPerPage }) {
         <>
             <ScrollRestoration/>
             <NavBar />
-            <div className="productsContainer">
-                <CardProduct currentItems={currentItems} />
-            </div>
-            <div className="paginationContainer">
-                <ReactPaginate
-                activeClassName={'item active '}
-                breakClassName={'item'}
-                breakLabel={'...'}
-                containerClassName={'pagination'}
-                disabledClassName={'disabled-page'}
-                marginPagesDisplayed={2}
-                pageClassName={'item pagination-page '}
-                nextLabel="Next >>"
-                onPageChange={handlePageClick}
-                pageRangeDisplayed={3}
-                pageCount={pageCount}
-                previousLabel="<< Previous"
-                renderOnZeroPageCount={null}
-            />
-            </div>
+            <div className="products-main">
+                <div className="productsBody">
+                    <section className="left">
+                        <div className="filterTitleBox">
+                            <h1 className="filterTitle">Filter</h1>
+                        </div>
+                        <div className="filterOptions first">
+                            <div className="option">
+                                <p>Woman's Clothing</p>
+                            </div>
+                            <div className="option">
+                                <p>Men's Clothing</p>
+                            </div>
+                            <div className="option">
+                                <p>Electronics</p>
+                            </div>
+                            <div className="option">
+                                <p>Jewelry</p>
+                            </div>
+                        </div>
+                    </section>
+                    <section className="right">
+                        <div className="productsContainer">
+                        <CardProduct currentItems={currentItems} />
+                        </div>
+                    </section>
+                </div>
+                <div className="paginationContainer">
+                    <ReactPaginate
+                    activeClassName={'item active '}
+                    breakClassName={'item'}
+                    breakLabel={'...'}
+                    containerClassName={'pagination'}
+                    disabledClassName={'disabled-page'}
+                    marginPagesDisplayed={2}
+                    pageClassName={'item pagination-page '}
+                    nextLabel="Next >>"
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={3}
+                    pageCount={pageCount}
+                    previousLabel="<< Previous"
+                    renderOnZeroPageCount={null}
+                    />
+                </div>
+            </div>    
             <Footer />
         </>
     )
