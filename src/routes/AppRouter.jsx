b0307ab994951aa48f9ecc2b64f2b7a6096ed0e3
Edgar Martínez <edgar.danmm@gmail.com>
2024-01-20 01:43:13 -0600

    update - fix routes and implemented usercontext

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,20 +1,20 @@
-import { BrowserRouter, Route, Routes} from "react-router-dom";
+import { Route, BrowserRouter, Routes } from 'react-router-dom'
 import Login from '/src/views/Login/Login';
 import Home from '/src/views/Home/Home';
 import { Products } from '/src/views/Products/Products';
-import UserContext from "/src/context/user-context";
+import { UserProvider } from '/src/context/user-context';
 
-const username = localStorage.getItem('username');
-
-const AppRouter = () =>{
-    return(
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home />}></Route>
-                <Route path="/Login" element={<Login/>}></Route>
-                <Route path="/Products" element={<Products itemsPerPage={6} />}></Route>
-            </Routes>
-        </BrowserRouter>
+const AppRouter = () => {
+    return (
+        <UserProvider>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                    <Route path="/Login" element={<Login/>}></Route>
+                    <Route path="/Products" element={<Products itemsPerPage={6} />}></Route>
+                </Routes>
+            </BrowserRouter>
+        </UserProvider>
     );
 };
 
